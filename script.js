b84bce485f6c4f05cfc86dfda7058d6c32e7f045
leoesf <leo.gautier@edf.fr>
2025-11-17 10:22:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function createPowerPoint() {
     slide.addImage({
       path: "EDF.png",   // ⚠️ Mets ce fichier dans ton GitHub !
       x: 0.1,            // coin bas-gauche
-      y: 7.1,            // très bas mais visible
+      y: 6.6,            // très bas mais visible
       w: 1.2,            // petit logo
       h: 0.55
     });
@@ -305,6 +305,7 @@ function createPowerPoint() {
   addInfoSlide();
   addChecklistSlides();
 }
+
 
 
 
